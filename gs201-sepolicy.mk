441f5022c19244bc9f18a7335ec4bef336b6944e
Oscar <Osm1019@gmail.com>
2023-05-29 12:34:08 -0700

    FixUp: Flipendo SEpolicy For Extreme Battery Saver mode

--- gs201-sepolicy.mk
+++ gs201-sepolicy.mk
@@ -24,3 +24,7 @@ BOARD_SEPOLICY_DIRS += device/google/gs201-sepolicy/health
 
 # Face HAL service
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs201-sepolicy/face
+
+# Flipendo
+BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/flipendo
+
